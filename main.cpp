8aabe693fe7215e4eb0011846222cc292c709343
nathaniw <nathaniw@uci.edu>
2015-08-20 15:39:37 -0700

    Create main.cpp
    Added main.cpp file.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "Location.h"
+#include "DistanceCalculator.h"
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	DistanceCalculator* DC;
+	DC = new DistanceCalculator();
+}
